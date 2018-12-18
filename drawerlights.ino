f0c602430da5433a8c98a82550c9dff38f5d5c36
Mark Fickett <mark.fickett@gmail.com>
2018-12-17 22:06:38 -0500

    factor into Drawer class

--- drawerlights.ino
+++ drawerlights.ino
@@ -1,14 +1,14 @@
 #include <Adafruit_NeoPixel.h>
-
+// https://github.com/NicoHood/PinChangeInterrupt
 #include <PinChangeInterrupt.h>
 
 #define NEO_PIXEL_PIN 9
 #define NUM_LEDS 60
 
-#define PIN_SWITCH_START 0
-#define NUM_SWITCHES 8
+#define NUM_DRAWERS 8
+#define LEDS_PER_DRAWER 4
 
-#define LEDS_PER_SWITCH 7
+#define PIN_SWITCH_START 0
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(
     NUM_LEDS, NEO_PIXEL_PIN, NEO_GRB + NEO_KHZ800);
@@ -19,29 +19,50 @@ void handleSwitchChange() {
   changed = true;
 }
 
+class Drawer {
+  private:
+    uint16_t start;
+    int switchPin;
+
+    void set(boolean on) {
+      uint32_t color = on ? strip.Color(255, 255, 0) : strip.Color(0, 0, 0);
+      for (uint16_t i = start; i < start + LEDS_PER_DRAWER; i++) {
+        strip.setPixelColor(i, color);
+      }
+    }
+  public:
+    // for array initialization; essentially unused
+    Drawer(): start(0), switchPin(0) {}
+
+    Drawer(uint16_t i_start, int i_switchPin):
+        start(i_start), switchPin(i_switchPin) {
+      pinMode(switchPin, INPUT_PULLUP);
+      attachPCINT(
+        digitalPinToPCINT(switchPin), handleSwitchChange, CHANGE);
+    }
+
+    void updateFromSwitch() {
+      // pullup + NC switch pressed by drawer
+      // therefore HIGH means the drawer is closed means LEDs off
+      set(digitalRead(switchPin) == LOW);
+    }
+};
+
+Drawer drawers[NUM_DRAWERS];
+
 void setup() {
   strip.begin();
-  changed = true;
-  for(int switchIndex = PIN_SWITCH_START;
-      switchIndex < PIN_SWITCH_START + NUM_SWITCHES;
-      switchIndex++) {
-    pinMode(switchIndex, INPUT_PULLUP);
-    attachPCINT(
-        digitalPinToPCINT(switchIndex), handleSwitchChange, CHANGE);
+  for(int i = 0; i < NUM_DRAWERS; i++) {
+    drawers[i] = Drawer(i * LEDS_PER_DRAWER, PIN_SWITCH_START + i);
   }
+  changed = true;
 }
 
 void loop() {
   if (changed) {
     delay(10); // debounce
-    for(int switchIndex = PIN_SWITCH_START;
-        switchIndex < PIN_SWITCH_START + NUM_SWITCHES;
-        switchIndex++) {
-      uint32_t color = digitalRead(switchIndex) == HIGH
-          ? strip.Color(255, 255, 80) : strip.Color(0, 0, 80);
-      for (int ledIndex = 0; ledIndex < LEDS_PER_SWITCH; ledIndex++) {
-        strip.setPixelColor(switchIndex * LEDS_PER_SWITCH + ledIndex, color);
-      }
+    for(int i = 0; i < NUM_DRAWERS; i++) {
+      drawers[i].updateFromSwitch();
     }
     strip.show();
     changed = false;
